a6104804f0f2d95fd1c59ba63efe5a6c9cda74b4
David Maulana <davidmaulana10k@gmail.com>
2020-07-31 01:46:49 +0700

    register screen

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,27 +1,61 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {Input} from '../../components';
+import React, {useState} from 'react';
+import {Text, View, ScrollView} from 'react-native';
+import {Input, Button} from '../../components';
 import {colors} from '../../utils/colors';
 import {IconBack, IllustrationsRegister} from '../../assets';
 
 export default function Register() {
+  const [form, setForm] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  const sendData = () => {
+    console.log('send data', form);
+  };
+
+  const onInputChange = (input, value) => {
+    setForm({
+      ...form,
+      [input]: value,
+    });
+  };
+
   return (
     <View style={styles.wrapper.page}>
-      <IconBack width={25} height={25} />
-      <IllustrationsRegister
-        width={106}
-        height={115}
-        style={styles.illustration}
-      />
-      <Text style={styles.text.desc}>
-        Mohon mengisi beberapa data untuk proses daftar anda
-      </Text>
-      <View style={styles.space(64)} />
-      <Input placeholder="nama lengkap" />
-      <View style={styles.space(33)} />
-      <Input placeholder="email" />
-      <View style={styles.space(33)} />
-      <Input placeholder="password" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <IconBack width={25} height={25} />
+        <IllustrationsRegister
+          width={106}
+          height={115}
+          style={styles.illustration}
+        />
+        <Text style={styles.text.desc}>
+          Mohon mengisi beberapa data untuk proses daftar anda
+        </Text>
+        <View style={styles.space(64)} />
+        <Input
+          placeholder="nama lengkap"
+          value={form.fullName}
+          onChangeText={(value) => onInputChange('fullName', value)}
+        />
+        <View style={styles.space(33)} />
+        <Input
+          placeholder="email"
+          value={form.email}
+          onChangeText={(value) => onInputChange('email', value)}
+        />
+        <View style={styles.space(33)} />
+        <Input
+          placeholder="password"
+          value={form.password}
+          onChangeText={(value) => onInputChange('password', value)}
+          secureTextEntry={true}
+        />
+        <View style={styles.space(83)} />
+        <Button title="Daftar" onPress={sendData} />
+      </ScrollView>
     </View>
   );
 }
